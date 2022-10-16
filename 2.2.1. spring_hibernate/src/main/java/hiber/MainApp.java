ddd9cd505191ae8e42ce05b88d6e8368d0016201
Strukov Alexandr <sasha_strukov_00@mail.ru>
2022-10-17 01:44:20 +0300

    2.2.1

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,34 +1,52 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
+        UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        Car lada = new Car("Lada", 69);
+        Car toyota = new Car("Toyota", 14);
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+        User user1 = new User("Sasha", "Strukov", "Sasha@mail.ru");
+        user1.setCar(lada);
 
-      context.close();
-   }
+        User user2 = new User("Vlad", "Tsel", "Vlad@mail.ru");
+        user2.setCar(toyota);
+
+        userService.add(user1);
+        userService.add(user2);
+
+        System.out.println("---------------------------------------");
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println("Car = " + user.getCar());
+            System.out.println();
+        }
+
+        System.out.println("---------------------------------------");
+
+        try {
+            User userGetByCar = userService.getUserByCar("Lada", 69);
+            System.out.println(userGetByCar);
+        } catch (Exception e) {
+            System.out.println("There is no user with such a machine in the database");
+        }
+        context.close();
+    }
 }
